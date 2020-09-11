9ca4a79953edb34d8d55fe6de7773bcf36b397cf
Adriana Jimenez <adriana.f.jimenez@gmail.com>
2020-09-10 23:11:35 -0400

    added styling, animations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function () {
+
     let $btns = $(".project-area .button-group button");
 
     $btns.click(function (e) {
@@ -12,8 +14,35 @@ $(document).ready(function () {
         return false
     });
 
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+    // $(function () {
+    //     $('[data-toggle="tooltip"]').tooltip();
+    // });
+
+    const seeKrivaya = document.querySelector("#seeKrivaya");
+    seeKrivaya.addEventListener('pointerenter', () => { 
+        var showKrivaya = document.getElementById('krivaya');
+        showKrivaya.classList.add("show");
+        console.log("test");
     });
- 
+
+    $(document).on('click','body *', () => {
+        var showKrivaya = document.getElementById('krivaya');
+        showKrivaya.classList.remove("show");
+    });
+
+
+
+    // //Obtaining the default helper
+    // var animationHelper = AniJS.getHelper();
+
+    // //Defining afterAnimationFunction
+    // animationHelper.beforeAnimationFunctionName = function (e, animationContext) {
+    //     if (true) {
+            
+    //         var seeKrivaya = document.getElementById('krivaya');
+    //         seeKrivaya.classList.toggle("show");
+
+    //         animationContext.run();
+    //     }
+    // }
 });
